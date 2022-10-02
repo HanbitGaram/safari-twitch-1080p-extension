1b82e6dd9002c8669e4f5c800467a90729f59d3d
한빛가람(ハンビッガラム) <webmaster@hanb.jp>
2022-10-02 20:11:05 +0900

    Initial Commit

---
+++ twitch-1080p/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  twitch-1080p
+//
+//  Created by Mirai Hasegawa on 2022/10/02.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ twitch-1080pUITests/twitch_1080pUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  twitch_1080pUITestsLaunchTests.swift
+//  twitch-1080pUITests
+//
+//  Created by Mirai Hasegawa on 2022/10/02.
+//
+
+import XCTest
+
+class twitch_1080pUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
